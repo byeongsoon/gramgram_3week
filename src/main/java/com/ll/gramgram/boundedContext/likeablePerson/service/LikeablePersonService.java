9921748f17e9f93581b2616db3cf81a2512f8a5b
byeongsoon <byeongsoon94@gmail.com>
2023-05-01 21:52:30 +0900

    feat : 호감표시/호감사유 변경 후 3시간 쿨타임 검증 구현

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonService.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonService.java
@@ -96,6 +96,10 @@ public class LikeablePersonService {
         if (actorInstaMemberId != fromInstaMemberId)
             return RsData.of("F-2", "권한이 없습니다.");
 
+        if (!likeablePerson.isModifyUnlocked()) {
+            return RsData.of("F-3", "호감취소는 호감표시 후 3시간 이후에 가능합니다.");
+        }
+
         return RsData.of("S-1", "삭제가능합니다.");
     }
 
@@ -209,6 +213,9 @@ public class LikeablePersonService {
             return RsData.of("F-2", "해당 호감표시를 취소할 권한이 없습니다.");
         }
 
+        if (!likeablePerson.isModifyUnlocked()) {
+            return RsData.of("F-3", "호감사유변경은 호감표시 후 3시간 이후에 가능합니다.");
+        }
 
         return RsData.of("S-1", "호감표시취소가 가능합니다.");
     }
