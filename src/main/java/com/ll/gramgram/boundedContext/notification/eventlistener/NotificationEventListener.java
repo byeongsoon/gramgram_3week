13c1cd68831196f34bd65af70b89e81c3024735e
byeongsoon <byeongsoon94@gmail.com>
2023-05-04 13:37:36 +0900

    feat : 호감표시 발생 알림 이벤트 처리

---
+++ src/main/java/com/ll/gramgram/boundedContext/notification/eventlistener/NotificationEventListener.java
@@ -0,0 +1,29 @@
+package com.ll.gramgram.boundedContext.notification.eventlistener;
+
+import com.ll.gramgram.base.event.EventAfterLike;
+import com.ll.gramgram.base.event.EventAfterModifyAttractiveType;
+import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
+import com.ll.gramgram.boundedContext.notification.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationEventListener {
+    private final NotificationService notificationService;
+
+    @EventListener
+    @Transactional
+    public void listen(EventAfterModifyAttractiveType event) {
+
+    }
+
+    @EventListener
+    public void listen(EventAfterLike event) {
+        LikeablePerson likeablePerson = event.getLikeablePerson();
+        notificationService.makeLike(likeablePerson);
+    }
+
+}
